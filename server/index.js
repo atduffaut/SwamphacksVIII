2465be605d933a8e42bad9a783f333aae2935275
Jim <jimsu@protonmail.com>
2022-01-28 20:42:06 -0500

    Add server.js

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+const io = new Server(httpServer, {});
+io.on("connection", (socket) => {});
+
+httpServer.listen(3000);
