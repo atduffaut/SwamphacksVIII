9e3e5fdf0cf7ed3caf61dfc0a8325b8de24c035f
Matthew Tave <tavematthew@gmail.com>
2022-01-29 13:45:19 -0500

    Added the dash

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -16,7 +16,10 @@ setTimeout(() => {
   let gameState = new PlayState(app);
 
   window.addEventListener('mousemove', function (event) {
-    gameState.player.move(event.x, event.y);
+    gameState.player.setMouse(event.x, event.y);
+  });
+  window.addEventListener('click', function (event) {
+    gameState.player.speedUp();
   });
 
   app.ticker.add((delta: number) => {
