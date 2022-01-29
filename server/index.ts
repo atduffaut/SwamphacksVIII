71ea812dcf359f51be3fdcb45e21d01075d30d01
Jim <jimsu@protonmail.com>
2022-01-28 20:56:46 -0500

    Add typescript

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,6 @@ const { Server } = require("socket.io");
 
 const httpServer = createServer();
 const io = new Server(httpServer, {});
-io.on("connection", (socket) => {});
+io.on("connection", (socket: any) => {});
 
 httpServer.listen(3000);
