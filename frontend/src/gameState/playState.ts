65ccd609cf7feedac573d71d89047d3d1c56c7b5
Alexander Duffaut <aduffaut@ufl.edu>
2022-01-29 16:13:57 -0500

    Co-authored-by: matbensch <matbensch@users.noreply.github.com> Co-authored-by: Matthew Tave <MatthewTave@users.noreply.github.com>

--- frontend/src/gameState/playState.ts
+++ frontend/src/gameState/playState.ts
@@ -21,9 +21,11 @@ export class PlayState extends GameState implements Updatable {
 
     for (let i = 0; i < 100; i++) {
       let rockPile = new RockPile(this);
-      rockPile.setX(Math.random() * (this.background.getWidth()*5 - rockPile.getWidth()));
+      rockPile.setX(
+        Math.random() * (this.background.getWidth() * 5 - rockPile.getWidth())
+      );
       rockPile.setY(
-        Math.random() * (this.background.getHeight()*5 - rockPile.getHeight())
+        Math.random() * (this.background.getHeight() * 5 - rockPile.getHeight())
       );
       // rockPile.setVy(0.1);
       this.addEntity(rockPile);
@@ -31,40 +33,74 @@ export class PlayState extends GameState implements Updatable {
 
     for (let i = 0; i < 100; i++) {
       let otherPlayer = new Player(this);
-      otherPlayer.setX(Math.random() * (this.background.getWidth()*5 - otherPlayer.getWidth()));
+      otherPlayer.setX(
+        Math.random() *
+          (this.background.getWidth() * 5 - otherPlayer.getWidth())
+      );
       otherPlayer.setY(
-        Math.random() * (this.background.getHeight()*5 - otherPlayer.getHeight())
+        Math.random() *
+          (this.background.getHeight() * 5 - otherPlayer.getHeight())
       );
       // rockPile.setVy(0.1);
       this.addEntity(otherPlayer);
     }
   }
 
+  isColliding(player, entity) {
+    let radiusPlayer = player.getWidth() / 2;
+    let radiusEntity = entity.getWidth() / 2;
+    let centerDistance = Math.sqrt(
+      Math.pow(player.getX() - entity.getX(), 2) +
+        Math.pow(player.getY() - entity.getY(), 2)
+    );
+
+    return centerDistance <= radiusEntity + radiusPlayer;
+  }
+
+  onCollide(player, entitiy) {}
+
+  handleCollisions() {
+    this.entities.forEach((entity) => {
+      if (this.isColliding(this.player, entity)) {
+        this.onCollide(this.player, entity);
+      }
+    });
+  }
+
   update(delta: number) {
     this.player.update(delta);
 
-    this.player.setX(Math.max(this.player.getX(),0));
-    this.player.setX(Math.min(this.player.getX(),5*this.background.getWidth()));
-    this.player.setY(Math.max(this.player.getY(),0));
-    this.player.setY(Math.min(this.player.getY(),5*this.background.getHeight()));
+    this.player.setX(Math.max(this.player.getX(), 0));
+    this.player.setX(
+      Math.min(this.player.getX(), 5 * this.background.getWidth())
+    );
+    this.player.setY(Math.max(this.player.getY(), 0));
+    this.player.setY(
+      Math.min(this.player.getY(), 5 * this.background.getHeight())
+    );
+
+    this.handleCollisions();
 
     this.entities.forEach((entity) => {
       entity.update(delta);
-      entity.setDisplayX(entity.getX() - this.player.getX() + this.player.getDisplayX());
-      entity.setDisplayY(entity.getY() - this.player.getY() + this.player.getDisplayY());
+      entity.setDisplayX(
+        entity.getX() - this.player.getX() + this.player.getDisplayX()
+      );
+      entity.setDisplayY(
+        entity.getY() - this.player.getY() + this.player.getDisplayY()
+      );
     });
   }
 
   draw(delta: number) {
     this.background.draw(delta);
-    
+
     this.player.move();
     this.player.setDisplayX(getRendererWidth() / 2);
     this.player.setDisplayY(getRendererHeight() / 2);
-    console.log(this.player.x);
 
     this.player.draw(delta);
-    
+
     this.entities.forEach((entity) => {
       entity.draw(delta);
     });
