2c742fc25361920accd50b5277aa87de4af5f64b
Alexander Duffaut <aduffaut@ufl.edu>
2022-01-29 23:08:50 -0500

    bouncing off of rocks

--- frontend/src/entity/player.ts
+++ frontend/src/entity/player.ts
@@ -5,7 +5,7 @@ import { Entity } from "./entity";
 export class Player extends Entity {
   speed: number = 1;
   cooldown: number = 0;
-
+  bouncer: number = 0;
   mouseX: number = 0;
   mouseY: number = 0;
 
@@ -18,56 +18,70 @@ export class Player extends Entity {
   update(delta: number) {
     super.update(delta);
   }
-  setMouse(x, y)
-  {
-    if(this.speed <= 1)
-    {
+  setMouse(x, y) {
+    if (this.speed <= 1 && this.bouncer <= 0) {
       this.mouseX = x;
       this.mouseY = y;
       this.rotate(x, y);
     }
   }
-  move()
-  {
-    let myX = this.getDisplayX()*SCALE;
-    let myY = this.getDisplayY()*SCALE;
+  move() {
+    let myX = this.getDisplayX() * SCALE;
+    let myY = this.getDisplayY() * SCALE;
 
-    let dist = Math.sqrt(Math.pow(myX - this.mouseX, 2) + Math.pow(myY - this.mouseY, 2));
+    let dist = Math.sqrt(
+      Math.pow(myX - this.mouseX, 2) + Math.pow(myY - this.mouseY, 2)
+    );
     dist = dist < 1 ? 1 : dist; // Fixes division by zero
 
-    this.setVx(((this.mouseX - myX) * this.speed) / dist);
-    this.setVy(((this.mouseY - myY) * this.speed) / dist);
-  }
-  speedUp()
-  {
-      if(this.cooldown <= 0)
-      {
-        this.speed = this.speed + 5;
-        this.cooldown = 5;
-      }
-  }
-  speedManager(delta)
-  {
-    if(this.speed > 1)
-    {
-      this.speed = this.speed - (delta/20);
+    if (this.bouncer <= 0) {
+      this.setVx(((this.mouseX - myX) * this.speed) / dist);
+      this.setVy(((this.mouseY - myY) * this.speed) / dist);
     }
-    if(this.speed < 1)
-    {
+  }
+  speedUp() {
+    if (this.cooldown <= 0) {
+      this.speed = this.speed + 5;
+      this.cooldown = 5;
+    }
+  }
+  speedManager(delta) {
+    if (this.speed > 1) {
+      this.speed = this.speed - delta / 20;
+    }
+    if (this.speed < 1) {
       this.speed = 1;
     }
-    if(this.cooldown > 0)
-    {
-      this.cooldown = this.cooldown - delta/60;
+    if (this.cooldown > 0) {
+      this.cooldown = this.cooldown - delta / 60;
     }
   }
   rotate(x, y) {
-    this.setRotation(Math.atan2(y - SCALE*this.getDisplayY(), x - SCALE*this.getDisplayX()) + 3*Math.PI/2);
+    this.setRotation(
+      Math.atan2(
+        y - SCALE * this.getDisplayY(),
+        x - SCALE * this.getDisplayX()
+      ) +
+        (3 * Math.PI) / 2
+    );
   }
   bounce() {
-
-  } 
+    if (this.bouncer <= 0) {
+      //do something to change direction
+      console.log("vx: ", this.vx, "vy: ", this.vy);
+      this.vx = -1 * this.vx;
+      this.vy = -1 * this.vy;
+      console.log("vx: ", this.vx, "vy: ", this.vy);
+      this.bouncer = 1;
+    }
+  }
+  bounceManager(delta) {
+    if (this.bouncer > 0) {
+      this.bouncer = this.bouncer - delta / 60;
+    }
+  }
   draw(delta: number) {
     this.speedManager(delta);
+    this.bounceManager(delta);
   }
 }
